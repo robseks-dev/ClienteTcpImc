09bb560f28f8cc88f4db7d64e6aeb7bb81977383
robseks-dev <robertocarlosberrio@gmail.com>
2024-09-06 21:55:38 -0500

    Commit add files

---
+++ src/robertoberrio/imc/cliente/Principal.java
@@ -0,0 +1,17 @@
+package robertoberrio.imc.cliente;
+
+import robertoberrio.imc.cliente.vistas.VentanaPrincipal;
+
+/**
+ *
+ * @author rober
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        VentanaPrincipal v = new VentanaPrincipal();
+        v.setLocationRelativeTo(null);
+        v.setVisible(true);
+    }
+    
+}
